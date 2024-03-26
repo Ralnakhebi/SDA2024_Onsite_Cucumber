6f82113d54f7f49a4d64bd7327acd4e7d170dffa
reema <ralnakhebi@hotmail.com>
2024-03-26 14:11:46 +0300

    day2

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -7,8 +7,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         glue = "stepdefinitions",
-        features = "src/test/resources/features",
-        tags = "  @ScenarioOutline",
+        features = "src/test/resources/features/dataTables.feature",
+        //tags = " @SO ",
         dryRun = false
 )
 public class Runner {
--- src/test/java/stepdefinitions/ScenarioOutlineStepDef.java
+++ src/test/java/stepdefinitions/ScenarioOutlineStepDef.java
@@ -3,22 +3,37 @@ package stepdefinitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import utilities.Driver;
 
 public class ScenarioOutlineStepDef {
     @Given("the user navigates to the Ebay homepage")
     public void theUserNavigatesToTheEbayHomepage() {
-
+        Driver.getDriver().get("https://www.ebay.com");
+        Driver.getDriver().navigate().refresh();
     }
 
     @When("the user searches for {string}")
-    public void theUserSearchesFor(String arg0, String arg1) {
+    public void theUserSearchesFor(String string) {
+        WebElement searchBox =Driver.getDriver().findElement(By.id("gh-ac"));
+        searchBox.sendKeys(string, Keys.ENTER);
     }
 
     @Then("the user verifies that the search results contain {string}")
-    public void theUserVerifiesThatTheSearchResultsContain(String arg0, String arg1) {
+    public void theUserVerifiesThatTheSearchResultsContain(String string) {
+        WebElement firstItem=Driver.getDriver().
+                findElement(By.xpath("//*[contains(@class,'srp-results')]" +
+                        "/li[@data-viewport]/div/div/a/div/span"));
+
+        Assert.assertTrue(firstItem.getText().toLowerCase().contains(string));
     }
 
     @When("I close the browser")
     public void iCloseTheBrowser() {
+        //Driver.closeDriver();
+
     }
 }

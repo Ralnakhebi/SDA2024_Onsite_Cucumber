fbeefa2eb22a6e3bba11018f1c03fc27c332bbad
reema <ralnakhebi@hotmail.com>
2024-04-18 09:07:51 +0300

    HW

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -7,8 +7,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         glue = "stepdefinitions",
-        features = "src/test/resources/features",
-        tags = " @Click_Buttons ",
+        features = "src/test/resources/features/dataTables.feature",
+        //tags = " @Click_Buttons ",
         plugin = {
                 "html:target/html-report/report.html",
                 "json:target/json-report/report.json",
